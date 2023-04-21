bdda7b1bda0ff5b765a16744d7a973c612371bb8
Ginanjar Utama <ginanjar.utama@gmail.com>
2023-04-21 20:13:20 +0700

    coba

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# Welcome to Streamlit!
+# LangChain-Streamlit Template
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
+This repo serves as a template for how to deploy a LangChain on Streamlit.
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+This repo contains an `main.py` file which has a template for a chatbot implementation.
+
+## Adding your chain
+To add your chain, you need to change the `load_chain` function in `main.py`.
+Depending on the type of your chain, you may also need to change the inputs/outputs that occur later on.
+
+## Deploy on Streamlit
+
+This is easily deployable on the Streamlit platform.
+Note that when setting up your StreamLit app you should make sure to add `OPENAI_API_KEY` as a secret environment variable.
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,45 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+"""Python file to serve as the frontend"""
 import streamlit as st
+from streamlit_chat import message
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from langchain.chains import ConversationChain
+from langchain.llms import OpenAI
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def load_chain():
+    """Logic for loading the chain you want to use should go here."""
+    llm = OpenAI(temperature=0)
+    chain = ConversationChain(llm=llm)
+    return chain
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+chain = load_chain()
 
-    points_per_turn = total_points / num_turns
+# From here down is all the StreamLit UI.
+st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
+st.header("LangChain Demo")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+
+
+def get_text():
+    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    return input_text
+
+
+user_input = get_text()
+
+if user_input:
+    output = chain.run(input=user_input)
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state["generated"]:
+
+    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
